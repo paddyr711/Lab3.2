c754143a8240e191cd72086780c5e78551b71db3
paddyr711 <pr711@bath.ac.uk>
2025-01-27 09:22:42 +0000

    MVP

--- main.py
+++ main.py
@@ -1,1 +1,262 @@
-print("Hello World!")
+import sqlite3
+
+# Define DBOperation class to manage all data into the database.
+# Give a name of your choice to the database
+
+class DBOperations:
+ sql_create_table_firsttime = "create table if not exists "
+
+
+ sql_create_table = "CREATE TABLE flights (flightID INTEGER PRIMARY KEY, flightOrigin VARCHAR(15), flightDestination VARCHAR(15), status VARCHAR(15))"
+
+
+ sql_insert = "INSERT INTO flights (flightID,flightOrigin, flightDestination, status)  VALUES (?, ?, ?, ?)"
+ sql_select_all = "select * from flights"
+ sql_search = "select * from flights where flightID = ?"
+ sql_alter_data = ""
+ sql_update_data = ""
+ sql_delete_data = ""
+ sql_drop_table = "DROP TABLE flights"
+
+
+ def __init__(self):
+   try:
+     self.conn = sqlite3.connect("DBFlights.db")
+     self.cur = self.conn.cursor()
+     self.cur.execute(self.sql_create_table_firsttime)
+     self.conn.commit()
+   except Exception as e:
+     print(e)
+   finally:
+     self.conn.close()
+
+
+ def get_connection(self):
+   self.conn = sqlite3.connect("DBFlights.db")
+   self.cur = self.conn.cursor()
+
+
+ def create_table(self):
+   try:
+     self.get_connection()
+     self.cur.execute(self.sql_create_table)
+     self.conn.commit()
+     print("Table created successfully")
+   except Exception as e:
+     print(e)
+   finally:
+     self.conn.close()
+
+
+ def insert_data(self):
+   try:
+     self.get_connection()
+
+
+     flight = FlightInfo()
+     flight.set_flight_id(int(input("Enter FlightID: ")))
+     flight.set_flight_origin(str(input("Enter FlightOrigin: ")))
+     flight.set_flight_destination(str(input("Enter FlightDestination: ")))
+     flight.set_status(str(input("Enter FlightStatus: ")))
+
+
+     self.cur.execute(self.sql_insert, tuple(str(flight).split("\n")))
+
+
+     self.conn.commit()
+     print("Inserted data successfully")
+   except sqlite3.IntegrityError as e:  # Catch the UNIQUE constraint error
+    print("Error: FlightID already exists. Please use a different ID.")
+   except Exception as e:
+     print(e)
+   finally:
+     self.conn.close()
+
+
+ def select_all(self):
+   try:
+     self.get_connection()
+     self.cur.execute(self.sql_select_all)
+     result = self.cur.fetchall()
+
+     for row in result:
+      print(row)  # Print each row
+
+
+     # think how you could develop this method to show the records
+
+
+   except Exception as e:
+     print(e)
+   finally:
+     self.conn.close()
+
+
+ def search_data(self):
+   try:
+     self.get_connection()
+     flightID = int(input("Enter FlightNo: "))
+     self.cur.execute(self.sql_search, tuple(str(flightID)))
+     result = self.cur.fetchone()
+     if type(result) == type(tuple()):
+       for index, detail in enumerate(result):
+         if index == 0:
+           print("Flight ID: " + str(detail))
+         elif index == 1:
+           print("Flight Origin: " + detail)
+         elif index == 2:
+           print("Flight Destination: " + detail)
+         else:
+           print("Status: " + str(detail))
+     else:
+       print("No Record")
+
+
+   except Exception as e:
+     print(e)
+   finally:
+     self.conn.close()
+
+
+ def update_data(self):
+   try:
+     self.get_connection()
+
+
+     # Update statement
+
+
+     if result.rowcount != 0:
+       print(str(result.rowcount) + "Row(s) affected.")
+     else:
+       print("Cannot find this record in the database")
+
+
+   except Exception as e:
+     print(e)
+   finally:
+     self.conn.close()
+
+
+
+
+# Define Delete_data method to delete data from the table. The user will need to input the flight id to delete the corrosponding record.
+
+
+ def delete_data(self):
+   try:
+     self.get_connection()
+
+
+     if result.rowcount != 0:
+       print(str(result.rowcount) + "Row(s) affected.")
+     else:
+       print("Cannot find this record in the database")
+
+
+   except Exception as e:
+     print(e)
+   finally:
+     self.conn.close()
+
+ def drop_table(self):
+   try:
+     self.get_connection()
+     self.cur.execute(self.sql_drop_table)
+     print('Table dropped!')
+   except Exception as e:
+     print(e)
+   finally:
+     self.conn.close()
+
+
+class FlightInfo:
+
+
+ def __init__(self):
+   self.flightID = 0
+   self.flightOrigin = ''
+   self.flightDestination = ''
+   self.status = ''
+
+
+ def set_flight_id(self, flightID):
+   self.flightID = flightID
+
+
+ def set_flight_origin(self, flightOrigin):
+   self.flightOrigin = flightOrigin
+
+
+ def set_flight_destination(self, flightDestination):
+   self.flightDestination = flightDestination
+
+
+ def set_status(self, status):
+   self.status = status
+
+
+ def get_flight_id(self):
+   return self.flightID
+
+
+ def get_flight_origin(self):
+   return self.flightOrigin
+
+
+ def get_flight_destination(self):
+   return self.flightDestination
+
+
+ def get_status(self):
+   return self.status
+
+
+ def __str__(self):
+   return str(
+     self.flightID
+   ) + "\n" + self.flightOrigin + "\n" + self.flightDestination + "\n" + str(
+     self.status)
+
+
+
+
+# The main function will parse arguments.
+# These argument will be definded by the users on the console.
+# The user will select a choice from the menu to interact with the database.
+
+
+while True:
+ print("\n Menu:")
+ print("**********")
+ print(" 1. Create table FlightInfo")
+ print(" 2. Insert data into FlightInfo")
+ print(" 3. Select all data from FlightInfo")
+ print(" 4. Search a flight")
+ print(" 5. Update data some records")
+ print(" 6. Delete data some records")
+ print(" 7. Drop table")
+ print(" 8. Exit\n")
+
+
+ __choose_menu = int(input("Enter your choice: "))
+ db_ops = DBOperations()
+ if __choose_menu == 1:
+   db_ops.create_table()
+ elif __choose_menu == 2:
+   db_ops.insert_data()
+ elif __choose_menu == 3:
+   db_ops.select_all()
+ elif __choose_menu == 4:
+   db_ops.search_data()
+ elif __choose_menu == 5:
+   db_ops.update_data()
+ elif __choose_menu == 6:
+   db_ops.delete_data()
+ elif __choose_menu == 7:
+   db_ops.drop_table() 
+ elif __choose_menu == 8:
+   exit(0)
+ else:
+   print("Invalid Choice")
+
